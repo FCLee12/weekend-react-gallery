6c25af1b63adf77bfbe1266ceac4a7c16a339bb9
Fue Lee <fueclee.12@gmail.com>
2023-03-23 16:28:02 -0500

    added database functionality

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "gallery" (
+	"id" SERIAL PRIMARY KEY,
+	"path" VARCHAR(250) NOT NULL,
+	"description" VARCHAR(250) NOT NULL,
+	"likes" INTEGER
+);
+
+INSERT INTO "gallery" ("path", "description", "likes")
+VALUES ('images/goat_small.jpg', 'Photo of a goat taken at Glacier National Park.', 0);
--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,15 @@
-const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
-];
+// const galleryItems = [
+//     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+// ];
 
-module.exports = galleryItems;
+const pg = require("pg");
+
+const pool = new pg.Pool ({
+    database: "react_gallery",
+    host: "localhost",
+    port:5432
+})
+
+module.exports = pool;
+
+// module.exports = galleryItems;
