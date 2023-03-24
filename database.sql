658af550f9b77ee55085b5af309c7bb029f6dc12
Fue Lee <fueclee.12@gmail.com>
2023-03-23 20:43:30 -0500

    minor edits to database.sql file

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE "react_gallery";
+
 CREATE TABLE "gallery" (
 	"id" SERIAL PRIMARY KEY,
 	"path" VARCHAR(250) NOT NULL,
@@ -5,5 +7,6 @@ CREATE TABLE "gallery" (
 	"likes" INTEGER
 );
 
+--Test Data
 INSERT INTO "gallery" ("path", "description", "likes")
 VALUES ('images/goat_small.jpg', 'Photo of a goat taken at Glacier National Park.', 0);
